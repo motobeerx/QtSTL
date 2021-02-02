d9bc368856807a883471234b9d17b28a6af7dcd4
Anton <43184533+motobeerx@users.noreply.github.com>
2021-02-02 10:43:02 +0300

    Transferred test in Qtest

--- main.cpp
+++ main.cpp
@@ -9,11 +9,10 @@
 /// Хотим переиспользование кода.
 
 /// Конкретные задачи
-/// 1. Переписать на QTest тесты
-/// 2. ++Понять https://en.cppreference.com/w/cpp/utility/optional
-/// 3. опционально - понять https://habr.com/ru/post/268013/
+/// 1. ++Переписать на QTest тесты
+/// 2. ++Понять https://en.cppreference.com/w/cpp/utility/optional +
 /// 4. Узнать синтаксис и семантику for +
-/// 5. Узнать про деструкторы, операторы new и delete. Как их использовать, чтобы не было утечек памяти.
+/// 5. ++Узнать про деструкторы, операторы new и delete. Как их использовать, чтобы не было утечек памяти.
 /// 6. опционально - узнать про тьюринг полноту
 /// 7. ++Реализовать классы List и Hash +
 /// 8. ++Поменять семантику find - на отрицательных индексах ищет элемент с конца. Сначала поменять тесты, потом код. +
@@ -21,37 +20,18 @@
 
 #include <iostream>
 #include <optional>
+#include <QtMath>
+
 #include <QTest>
 #include <QElapsedTimer>
 #include <QVector>
 #include <QHash>
 
-
-/// Макрос для тестовых проверок.
-#define ASSERT_EQUALS(caseName, actual, expected, onFailureText) \
-    if(actual == expected)                                       \
-    {                                                            \
-        std::cout << "\033[1;32m OK    \033[0m: ";               \
-        std::cout << caseName << std::endl;                      \
-    }                                                            \
-    else                                                         \
-    {                                                            \
-        std::cout << "\033[1;31m FAIL \033[0m: ";               \
-        std::cout << caseName << std::endl;                      \
-        std::cout << onFailureText << std::endl;                 \
-        std::cout << "	actual: " << actual << std::endl;        \
-        std::cout << "	expected: " << expected << std::endl;    \
-        std::cout << std::endl;                                  \
-    }
-
-
 class IndexedCollection
 {
 public:
-    virtual ~IndexedCollection(){}
-
+    virtual ~IndexedCollection() = default;
     virtual void add(const int element) = 0;
-
     virtual std::optional<int> find(const int index) const = 0;
 
 };
@@ -60,141 +40,168 @@ public:
 class Vector : public IndexedCollection
 {
 public:
-    void add(const int element)
+    void add(const int element) override
     {
-        _elements << element;
+        elements_ << element;
     }
 
-    std::optional<int> find(const int index) const
+    std::optional<int> find(const int index) const override
     {
-        if(_elements.isEmpty()){
+        if(elements_.isEmpty() or ((index < elements_.size()) xor (index >= -elements_.size()))){
             return std::nullopt;
         }
 
-        int reduced_index = index < 0 ? ((index % _elements.size()) + _elements.size()) : index;
+        int reducedIndex = index < 0 ? (index + elements_.size()) : index;
 
-        if(reduced_index >= _elements.size()){
-            return std::nullopt;
-        }
-        return _elements.value(reduced_index);
+        return elements_.value(reducedIndex);
 
     }
 
 private:
-    QVector<int> _elements;
+    QVector<int> elements_;
 };
 
 
 class List : public IndexedCollection
 {
-    void add(const int element)
+    void add(const int element) override
     {
-        _elements << element;
+        elements_ << element;
     }
 
-    std::optional<int> find(const int index) const
+    std::optional<int> find(const int index) const override
     {
-        if(_elements.isEmpty()){
+        if(elements_.isEmpty() or ((index < elements_.size()) xor (index >= -elements_.size()))){
             return std::nullopt;
         }
 
-        int reduced_index = index < 0 ? ((index % _elements.size()) + _elements.size()) : index;
+        int reducedIndex = index < 0 ? (index + elements_.size()) : index;
 
-        if(reduced_index >= _elements.size()){
-            return std::nullopt;
-        }
-        return _elements.value(reduced_index);
+        return elements_.value(reducedIndex);
+
     }
 
 private:
-    QList<int> _elements;
+    QList<int> elements_;
 };
 
 
 class Hash : public IndexedCollection
 {
-    void add(const int element)
+    void add(const int element) override
     {
-        _elements.insert(_elements.size(), element);
+        elements_.insert(elements_.size(), element);
     }
 
-    std::optional<int> find(const int index) const
+    std::optional<int> find(const int index) const override
     {
-        if(_elements.isEmpty()){
+        if(elements_.isEmpty() or ((index < elements_.size()) xor (index >= -elements_.size()))){
             return std::nullopt;
         }
 
-        int reduced_index = index < 0 ? ((index % _elements.size()) + _elements.size()) : index;
+        int reducedIndex = index < 0 ? (index + elements_.size()) : index;
 
-        if(reduced_index >= _elements.size()){
-            return std::nullopt;
-        }
-        return _elements.value(reduced_index);
+        return elements_.value(reducedIndex);
+
     }
 
 private:
-    QHash<int, int> _elements;
+    QHash<int, int> elements_;
 };
 
 
 class TestIndexedCollection: public QObject{
 
 
-    int const _numberOfElements = 1000000;
+    int const numberOfElements_ = 1000000;
+    IndexedCollection* collection_;
+
+    void fillCollection(int amountOfElements){
+        for(int i = 0; i != amountOfElements; ++i){
+            collection_->add(i*i);
+        }
+    }
+
+    qint64 benchmarkAdd(){
+        qint64 period;
+        QElapsedTimer time;
+        time.restart();
+        for(int i = 0; i < numberOfElements_; ++i)
+        {
+            collection_->add(i);
+        }
+        period = time.elapsed();
+        return period;
+    }
+
+    qint64 benchmarkFind(){
+        qint64 period;
+        QElapsedTimer time;
+        time.restart();
+        for(int i = 0; i < numberOfElements_; ++i)
+        {
+            collection_->find(i);
+        }
+
+        period = time.elapsed();
+        return period;
+    }
 
 public:
-    void smokeTest(IndexedCollection *collection){
-        ASSERT_EQUALS("1)  collection->find(0) from empty collection", (collection->find(0) == std::nullopt), true, "");
-        ASSERT_EQUALS("2)  collection->find(87) from empty collection", (collection->find(87) == std::nullopt), true, "");
-        ASSERT_EQUALS("3)  collection->find(9999999) from empty collection", (collection->find(9999999) == std::nullopt), true, "");
-        ASSERT_EQUALS("4)  collection->find(-50) from empty collection", (collection->find(-50) == std::nullopt), true, "");
-
-        collection->add(42);
-        ASSERT_EQUALS("5)  collection->find(0)  after adding 0 element", (collection->find(0) == 42), true, "");
+    TestIndexedCollection(IndexedCollection *collection): collection_(collection){};
 
 
-        ASSERT_EQUALS("6)  collection->find(1) when 1 element not added", (collection->find(1) == std::nullopt), true, "");
-        ASSERT_EQUALS("7)  collection->find(0) when 1 element not added", (collection->find(0) == 42), true, "");
-        collection->add(99);
-        ASSERT_EQUALS("8)  collection->find(1) after adding 1 element", (collection->find(1) == 99), true, "");
-        ASSERT_EQUALS("9)  collection->find(0) after adding 1 element", (collection->find(0) == 42), true, "");
-        ASSERT_EQUALS("10) collection->find(-1) after adding 1 element", (collection->find(-1) == 99), true, "");
+private slots:
+    void testEmptyCollection_data(){
+        QTest::addColumn<int>("Index");
+        QTest::addColumn<std::optional<int>>("Expected Result");
+
+        QTest::newRow(typeid(*collection_).name()) << 0 << std::nullopt;
+        QTest::newRow(typeid(*collection_).name()) << 1 << std::nullopt;
+        QTest::newRow(typeid(*collection_).name()) << -1 << std::nullopt;
+        QTest::newRow(typeid(*collection_).name()) << 4 << std::nullopt;
+        QTest::newRow(typeid(*collection_).name()) << -3 << std::nullopt;
+        QTest::newRow(typeid(*collection_).name()) <<  1000 << std::nullopt;
+        QTest::newRow(typeid(*collection_).name()) << -9735 << std::nullopt;
+
+
     }
 
-    qint64 benchmarkAdd(IndexedCollection *collection){
-        qint64 period;
-        QElapsedTimer time;
-        time.restart();
-        for(int i = 0; i < _numberOfElements; ++i)
-        {
-            collection->add(i);
-        }
-        period = time.elapsed();
-        return period;
+    void testEmptyCollection(){
+        QFETCH(int, index);
+        QFETCH(std::optional<int>, expected);
+
+        QCOMPARE(collection_->find(index), expected);
     }
 
-    qint64 benchmarkFind(IndexedCollection *collection){
-        qint64 period;
-        QElapsedTimer time;
-        time.restart();
-        for(int i = 0; i < _numberOfElements; ++i)
-        {
-            collection->find(i);
-        }
+    void testFilledCollection_data(){
+        QTest::addColumn<int>("Index");
+        QTest::addColumn<std::optional<int>>("Expected Result");
 
-        period = time.elapsed();
-        return period;
+
+        int numberOfElements = 6;
+        this->fillCollection(numberOfElements);
+
+        QTest::newRow(typeid(*collection_).name()) << 0 << 0;
+        QTest::newRow(typeid(*collection_).name()) << 1 << 1;
+        QTest::newRow(typeid(*collection_).name()) << -1 << qPow(numberOfElements - 1, 2);
+        QTest::newRow(typeid(*collection_).name()) << numberOfElements - 1 << qPow(numberOfElements - 1, 2);
+        QTest::newRow(typeid(*collection_).name()) << numberOfElements - 2 << qPow(numberOfElements, 2);
+        QTest::newRow(typeid(*collection_).name()) << numberOfElements <<  std::nullopt;
+        QTest::newRow(typeid(*collection_).name()) << -numberOfElements << 0;
+        QTest::newRow(typeid(*collection_).name()) << 1 - numberOfElements << 1;
+        QTest::newRow(typeid(*collection_).name()) << -1 - numberOfElements << std::nullopt;
+        QTest::newRow(typeid(*collection_).name()) <<  1000 << std::nullopt;
+        QTest::newRow(typeid(*collection_).name()) << -9735 << std::nullopt;
+
     }
 
-    void testCollection(IndexedCollection *collection){
-        std::cout << typeid(*collection).name() << std::endl;
+    void testFilledCollection(){
 
-        this->smokeTest(collection);
-        std::cout << typeid(*collection).name() << ": " << _numberOfElements << " elements inserted for " << this->benchmarkAdd(collection) << " ms" << std::endl;
-        std::cout << typeid(*collection).name() << ": " <<_numberOfElements << " elements found for " << this->benchmarkFind(collection) << " ms" << std::endl;
+        QFETCH(int, index);
+        QFETCH(std::optional<int>, expected);
 
-        delete collection;
-        std::cout << std::endl;
+        QCOMPARE(collection_->find(index), expected);
     }
 
 };
@@ -203,10 +210,9 @@ public:
 
 int main()
 {
-    TestIndexedCollection test;
-    test.testCollection(new Vector());
-    test.testCollection(new List());
-    test.testCollection(new Hash());
+     QTest::qExec(new TestIndexedCollection(new Vector));
+     QTest::qExec(new TestIndexedCollection(new List));
+     QTest::qExec(new TestIndexedCollection(new Hash));
 
     return 0;
 }
