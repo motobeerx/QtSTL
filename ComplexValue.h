737e65ea5d39e254e7c43b723ce0e27749224000
Александр Сапожников (Aleksandr Sapozhnikov) <alexxandrsap@mail.ru>
2021-02-05 08:24:14 +0300

    ревью ComplexValue.h

--- ComplexValue.h
+++ ComplexValue.h
@@ -5,6 +5,11 @@
 
 struct ComplexValue
 {
+    /// review: значения по умолчанию нужно максимально избегать
+    /// тут я не вижу причин для их использования. И почему -1 ? 
+    
+    /// review: если конструктор имеет 1 аргумент, то нужно прописать ему explicit. 
+    /// это позволит избежать неявного преобразования int -> ComplexValue, которое является некорректным.
     ComplexValue(int const key = -1);
     bool operator==(ComplexValue const &other) const;
     bool operator<(ComplexValue const &other) const;
